b10742ff570369cb40abb30ee56fd13b1cd5958b
Choestelus Suchdoge <sprintf.null@gmail.com>
2015-08-24 23:20:25 +0700

    prototyped BFS

--- solver.go
+++ solver.go
@@ -1,18 +1,42 @@
 package main
 
-func (et EightTable) BFSSolve() {
-	moves := et.Movables()
-	// TODO: compare table in each recursion
-	if moves.Down {
-		et.MoveDown()
+func BFSSolve(solutionTable EightTable, puzzleTable EightTable, moves []Moves) []Moves {
+	if solutionTable == puzzleTable {
+		return moves
 	}
-	if moves.Up {
-		et.MoveUp()
+
+	m := puzzleTable.Movables()
+
+	if m.Down {
+		err := puzzleTable.MoveDown()
+		if err != nil {
+			panic(err)
+		}
+		moves = append(moves, Moves{Down: true})
+		BFSSolve(solutionTable, puzzleTable, moves)
 	}
-	if moves.Left {
-		et.MoveLeft()
+	if m.Up {
+		err := puzzleTable.MoveUp()
+		if err != nil {
+			panic(err)
+		}
+		moves = append(moves, Moves{Up: true})
+		BFSSolve(solutionTable, puzzleTable, moves)
 	}
-	if moves.Right {
-		et.MoveRight()
+	if m.Left {
+		err := puzzleTable.MoveLeft()
+		if err != nil {
+			panic(err)
+		}
+		moves = append(moves, Moves{Left: true})
+		BFSSolve(solutionTable, puzzleTable, moves)
+	}
+	if m.Right {
+		err := puzzleTable.MoveRight()
+		if err != nil {
+			panic(err)
+		}
+		moves = append(moves, Moves{Right: true})
+		BFSSolve(solutionTable, puzzleTable, moves)
 	}
 }
